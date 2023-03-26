3b57c84ca49333d4f5f82436325c40a82e70980b
rizemon <tan.jia.le.98@gmail.com>
2023-03-26 05:42:57 -0400

    Add /captured, /redirected, /reflected routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from json import dumps
 
 app = Flask(__name__)
+
+captured_contents = []
 
 @app.route("/", methods=["GET"])
 def index():
@@ -13,3 +16,22 @@ def unauthorized():
 @app.route("/protected", methods=["GET"])
 def protected():
     return "Protected page"
+
+@app.route("/captured", methods=["GET", "POST"])
+def captured():
+    if request.method == "POST":
+        content = request.form["content"]
+        captured_contents.append(content)
+        return "Appended: " + dumps([content])
+        
+    return dumps(captured_contents, indent=4)
+
+@app.route("/redirected", methods=["GET"])
+def redirected():
+    host = request.headers["host"]
+    return redirect("http://" + host, code=302)
+
+@app.route("/reflected", methods=["GET"])
+def reflected():
+    user_agent = request.headers["user-agent"]
+    return "User-Agent: " + user_agent
